826830568206e073693bf4e9ec3c340c1931a7e0
Ethan Murray <ethanmurray3@icloud.com>
2026-05-17 19:22:11 -0700

    added lsp to nvim config

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {},
+        pyright = {},
+        rust_analyzer = {},
+        zls = {},
+      },
+    },
+  },
+}
